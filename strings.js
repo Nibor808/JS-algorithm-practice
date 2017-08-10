41626ca9f1c449b59dc8d5d2b6e5bf4bf51314de
robin <re@incubo.ca>
2017-08-10 16:15:03 -0400

    test

--- strings.js
+++ strings.js
@@ -94,7 +94,30 @@ function mutation(arr) {
 }
 mutation(['hello', 'hl']);
 
+//DECRYPT ROT13 CODED STRING
+function rot13(str) {
+  str = str.split('');
 
+  const resultArr = str.map((item, index) => {
+    if (item.charCodeAt(0) >= 32 && item.charCodeAt(0) <= 64) {
+      return item.charCodeAt(0);
+    }else if (item.charCodeAt(0) - 13 < 65){
+      return item.charCodeAt(0) + 13
+    }else {
+      return item.charCodeAt(0) - 13
+    }
+  });
+
+  console.log(String.fromCharCode.apply(null, resultArr));
+  return String.fromCharCode.apply(null, resultArr);
+}
+
+function blegh() {
+  console.log('another test string')
+}
+function blegh() {
+  console.log('another test string')
+}
 function blegh() {
   console.log('another test string')
 }
